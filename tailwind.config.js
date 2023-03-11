f2973a44ec3fb61e572e8d9b71b8e68449bdc350
tasniul-hasan <tasnimulh453@gmail.com>
2023-03-11 17:41:20 +0600

    login and recovery page completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html","./user_details.html","./message.html"],
+  content: [
+    "index.html",
+    "./user_details.html",
+    "./message.html",
+    "./dashboard_login.html",
+    "./recovery_account.html"
+  ],
   theme: {
     extend: {},
   },
